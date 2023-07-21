39438c027b3dfc9f3478b4df544e6fada238388a
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-07-21 12:04:57 +0800

    deleted data file
    forgot to put in gitignore earlier

--- supportJSFiles/unanswered.js
+++ supportJSFiles/unanswered.js
@@ -1,2 +1,15 @@
 const questionsDiv = document.getElementById("questionsDiv")
+// const token = 
+questionData = null
 
+function renderData() {
+    if (questionData == null) {
+        return
+    }
+
+    console.log("rendering...")
+}
+
+function fetchData() {
+
+}
